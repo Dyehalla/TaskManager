90b43d7fb4a33fca7fa9030956c2ec0495843ad9
Igor <igmih206@gmail.com>
2025-03-14 10:58:00 +0700

    Подготовил ветку

--- main.cpp
+++ main.cpp
@@ -1,12 +1,7 @@
-#include <QApplication>
-#include "mainwindow.h"
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);  // Создаем приложение Qt
-
-    MainWindow window;             // Создаем главное окно
-    window.show();                 // Показываем окно
-
-    return app.exec();             // Запускаем цикл обработки событий
+    std::cout << "си плас плас коллеги";
+    return 0;   
 }
